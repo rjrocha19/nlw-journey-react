b40a4e5ca4dabfb8e14750ba208bc6a41cf06cca
rjrocha19 <rodolfo_rj@live.com>
2024-07-23 18:02:10 -0300

    update

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,8 +1,8 @@
-import { Calendar, MapPin, Settings2 } from "lucide-react";
+import { Calendar, MapPin, Plus, Settings2 } from "lucide-react";
 
 export function TripDetailsPage() {
   return (
-    <div className="max-w-6xl px-6 py-10 mx-auto">
+    <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
       <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
         <div className="flex items-center gap-2">
           <MapPin className="size-5 text-zinc-400" />
@@ -28,6 +28,10 @@ export function TripDetailsPage() {
         <div className="flex-1">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
+            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+              <Plus className="size-5" />
+              Cadastrar ativiadade
+            </button>
           </div>
         </div>
         <div className="w-88"></div>
