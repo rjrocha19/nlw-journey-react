a7af7d3af8a76dc107cd706f466e24d6339f8194
Rodolfo Rocha <rjrocha@Rodolfos-iMac.local>
2024-07-25 07:00:09 -0300

    update

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,15 +1,15 @@
-import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, UserCog } from "lucide-react";
+import { Calendar, CircleCheck, CircleDashed, Clock, Link2, Mail, MapPin, Plus, Settings2, Tag, User, UserCog, X } from "lucide-react";
 import { useState } from "react";
 
 export function TripDetailsPage() {
-  const [isCreateAtionModalOpen, setIsCreateAtionModalOpen] = useState(false)
+  const [isCreateActionModalOpen, setIsCreateActionModalOpen] = useState(false)
 
   function handleOpenCreateActionModal() {
-    setIsCreateAtionModalOpen(true)
+    setIsCreateActionModalOpen(true)
   }
 
   function handleCloseCreateActionModal() {
-    setIsCreateAtionModalOpen(false)
+    setIsCreateActionModalOpen(false)
   }
 
 
@@ -41,7 +41,7 @@ export function TripDetailsPage() {
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
-            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            <button onClick={handleOpenCreateActionModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
               <Plus className="size-5" />
               Cadastrar ativiadade
             </button>
@@ -130,6 +130,46 @@ export function TripDetailsPage() {
           </div>
         </div>
       </main>
+
+      {isCreateActionModalOpen && (
+        <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
+          <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+            <div className='space-y-2'>
+              <div className='flex items-center justify-between'>
+                <h2 className='text-lg font-semibold'>Cadastrar atividade</h2>
+                <button type='button' onClick={handleCloseCreateActionModal}>
+                  <X className='size-5 text-zinc-400'/>
+                </button>
+              </div>          
+              <p className='text-sm text-zinc-400'>
+                Todos convidados podem visualizar as atividades.
+              </p>
+            </div>
+              
+            <form  className='space-y-3'>
+              <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+                <Tag className='size-5 text-zinc-400' />
+                <input name="title" placeholder="Qual a atividade?" className="bg-transparent text-lg place-holder-zinc-400 outline-none flex-1"/>
+              </div>
+    
+              <div className="flex item gap-2">
+                <div className='h-14 px-4 flex-1 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+                  <Calendar className='size-5 text-zinc-400' />
+                  <input type="datetime-local" name="email" placeholder="20 de agosto" className="bg-transparent text-lg place-holder-zinc-400 outline-none flex-1"/>
+                </div>
+                <div className='h-14 px-4 w-36 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+                  <Clock className='size-5 text-zinc-400' />
+                  <input type="datetime-local" name="email" placeholder="Horário" className="bg-transparent text-lg place-holder-zinc-400 outline-none flex-1"/>
+                </div>
+              </div>
+    
+              <button  type='submit'  className='bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-600'>
+                Salvar atividade
+              </button>   
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
